617a701bd456d20a6268fddf3fc704d3cfc552da
Naveenkumar S E <naveen42moon@gmail.com>
2023-05-07 21:23:22 +0530

    read-completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {db} from './firebase';
+import {query,collection, onSnapshot} from 'firebase/firestore';
+
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -12,11 +15,27 @@ const style = {
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
   button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`
+  count: `text-center p-2 `
 }
 
 function App() {
-  const [todos,setTodos] = useState(['learn react','Grind leetcode']);
+  const [todos,setTodos] = useState([]);
+
+  //create todo 
+
+  //read todo 
+
+  useEffect(()=>{
+      const q = query(collection(db,'todos'))
+      const unSubscribe = onSnapshot(q,(querySnapshot)=>{
+        let todoArr = []
+        querySnapshot.forEach((doc)=>{
+          todoArr.push({...doc.data(),id:doc.id})
+        });
+        setTodos(todoArr);
+      })
+      return ()=> unSubscribe(); 
+  },[])
   return (
     <div className={style.bg}>
         <div className={style.containers}>
